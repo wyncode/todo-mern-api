a8b37407c43651df31053ef8dadab40fe28d28d3
leopolicastro <lpolicastro@pm.me>
2020-04-01 19:18:37 -0400

    refactor model objects into their own files, create task and user post routes

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+require('./db/mongoose');
+const User = require('./models/user');
+const Task = require('./models/task');
+
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch(e => {
+      res.send(e);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Express server is up on port ${port}`);
