936e480f9f1ef2aa52a577ad87895f378abc211c
leopolicastro <lpolicastro@pm.me>
2020-04-01 22:32:31 -0400

    refactor to async await

--- index.js
+++ index.js
@@ -14,43 +14,38 @@ const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .then(users => {
-      res.status(200).send(users);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
   if (mongoose.Types.ObjectId.isValid(_id)) {
-    User.findById(_id)
-      .then(user => {
-        if (!user) {
-          // The gotcha here is that this will only trigger if the param sent is 12 bits (12 character string)
-          return res.status(404).send();
-        }
-        res.send(user);
-      })
-      .catch(e => {
-        console.log(e.toString());
-        res.status(500).send();
-      });
+    try {
+      const user = await User.findById(_id);
+      if (!user) {
+        // The gotcha here is that this will only trigger if the param sent is 12 bits (12 character string)
+        return res.status(404).send();
+      }
+      res.send(user);
+    } catch (e) {
+      res.status(500).send();
+    }
   } else {
     res.status(400).send('Not a valid user id');
   }
@@ -58,48 +53,40 @@ app.get('/users/:id', (req, res) => {
 
 // ******************************************** //
 
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then(tasks => {
-      res.status(200).send(tasks);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id;
-  console.log(_id);
   if (mongoose.Types.ObjectId.isValid(_id)) {
-    Task.findById(_id)
-      .then(task => {
-        if (!task) {
-          // The gotcha here is that this if will only trigger if the param sent is 12 bits (12 character string)
-          return res.status(404).send();
-        }
-        res.send(task);
-      })
-      .catch(e => {
-        console.log(e.toString());
-        res.status(500).send();
-      });
+    try {
+      const task = await Task.findById(_id);
+      res.send(task);
+      if (!task) {
+        return res.status(404).send();
+      }
+    } catch (e) {
+      res.status(500).send();
+    }
   } else {
     res.status(400).send('Not a valid task id');
   }
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
-
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+  try {
+    task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 app.listen(port, () => {
