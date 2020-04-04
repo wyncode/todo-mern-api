903aff33ffa0f25a918a7883b1a6436a48ec32e2
Leo <lpolicastro@pm.me>
2020-04-04 10:01:09 -0400

    logout user routes

--- routes/user.js
+++ routes/user.js
@@ -36,6 +36,33 @@ router.post('/users/login', async (req, res) => {
 });
 
 // ***********************************************//
+// Logout a user
+// ***********************************************//
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// ***********************************************//
+// Logout all devices
+// ***********************************************//
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+// ***********************************************//
 // Get current users
 // ***********************************************//
 
