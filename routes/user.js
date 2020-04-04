5fe2352a27bae99041b89977d6e0db692ef14aec
Leo <lpolicastro@pm.me>
2020-04-04 08:09:36 -0400

    add initial login route and method

--- routes/user.js
+++ routes/user.js
@@ -18,6 +18,22 @@ router.post('/users', async (req, res) => {
 });
 
 // ***********************************************//
+// Login a user
+// ***********************************************//
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// ***********************************************//
 // Get all users
 // ***********************************************//
 
@@ -55,7 +71,7 @@ router.get('/users/:id', async (req, res) => {
 router.patch('/users/:id', async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
-  const isValidOperation = updates.every(update =>
+  const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update)
   );
   if (!isValidOperation) {
@@ -63,8 +79,7 @@ router.patch('/users/:id', async (req, res) => {
   }
   try {
     const user = await User.findById(req.params.id);
-
-    updates.forEach(update => (user[update] = req.body[update]));
+    updates.forEach((update) => (user[update] = req.body[update]));
     await user.save();
     if (!user) {
       return res.status(404).send();
