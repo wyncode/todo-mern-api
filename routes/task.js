4e7ba810765360b8ee8119123599a9e5c3d79e3f
Leo <lpolicastro@pm.me>
2020-04-04 12:10:45 -0400

    sort through completed and incomplete tasks

--- routes/task.js
+++ routes/task.js
@@ -9,8 +9,19 @@ const Task = require('../models/task');
 // Get all tasks
 // ***********************************************//
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user
+      .populate({
+        path: 'tasks',
+        match
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
