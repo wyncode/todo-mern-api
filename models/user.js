2d3963ec813ea5b974460cca4d0b1a563f6946e8
Leo Policastro <43428385+leopolicastro@users.noreply.github.com>
2020-04-25 09:29:09 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -81,7 +81,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, { expiresIn: '7 days' });
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
