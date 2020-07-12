9a6a73a26f149a87b2596c90781353abc0b37fa6
wkennedy210@gmail.com <6560256wK1>
2020-07-11 20:05:24 -0400

    add due date filters from laz's code, refactor so it aligns with my code. Add sweet alert (for leo lol)

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Container, Table } from 'react-bootstrap';
+import axios from 'axios';
 import Task from './Task';
+import Search from './Search';
+import { AuthContext } from '../context/AuthContext';
 
 const TaskList = () => {
+  const token = localStorage.getItem('token');
+  const [search, setSearch] = useState('');
+  const {
+    tasks,
+    setTasks,
+    setFilteredTasks,
+    filteredTasks,
+    currentFilter
+  } = useContext(AuthContext);
+
+  // initital render will set all the todos to the `tasks` state
+  useEffect(() => {
+    axios
+      .get('/tasks?sortBy=dueDate:asc', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then((response) => {
+        setTasks(response.data);
+      })
+      .catch((error) => {
+        console.log(`Tasks Request Error: `, error);
+      });
+  }, [token, setTasks, tasks]);
+
+  // whenever a filter is applied, it updates the tasks to render
+  useEffect(() => {
+    if (search.length > 1) {
+      setFilteredTasks(
+        tasks.filter((task) => {
+          return task.description.toLowerCase().includes(search.toLowerCase());
+        })
+      );
+    } else if (currentFilter.length < 1) {
+      setFilteredTasks(tasks);
+    }
+  }, [search, setFilteredTasks, tasks, currentFilter.length, filteredTasks]);
+
   return (
     <Container>
+      <Search setSearch={setSearch} />
       <Table>
         <thead>
           <tr>
@@ -14,7 +57,7 @@ const TaskList = () => {
           </tr>
         </thead>
         <tbody>
-          <Task />
+          <Task tasks={filteredTasks} />
         </tbody>
       </Table>
     </Container>
