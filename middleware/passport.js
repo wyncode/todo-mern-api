410712eaf663a5a2243e36d477c504fd2ac667c6
Leo <lpolicastro@pm.me>
2020-07-14 21:57:20 -0400

    add cookieparser and passport

---
+++ middleware/passport.js
@@ -0,0 +1,27 @@
+const passport = require('passport'),
+  JwtStrategy = require('passport-jwt').Strategy,
+  ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// ******************************
+// JWT Strategy
+// ******************************
+
+let jwtOptions = {
+  jwtFromRequest: (req) => {
+    return req?.cookies?.jwt || ExtractJwt.fromAuthHeaderWithScheme('JWT')(req);
+  },
+  secretOrKey: process.env.JWT_SECRET
+};
+
+passport.use(
+  'jwt',
+  new JwtStrategy(jwtOptions, async (jwtPayload, done) => {
+    if (Date.now() > jwtPayload.expires) {
+      return done(null, false, { message: 'jwt expired' });
+    }
+    let { iat, exp, ...userData } = jwtPayload;
+    return done(null, userData);
+  })
+);
+
+module.exports = passport;
--- models/user.js
+++ models/user.js
@@ -81,7 +81,11 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, { expiresIn: '7 days' });
+  const token = jwt.sign(
+    { _id: user._id.toString(), name: user.name },
+    process.env.JWT_SECRET,
+    { expiresIn: '7 days' }
+  );
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
