04b023d37b98992cd4603cd82ca2d8229a8ede73
Leo <lpolicastro@pm.me>
2020-07-16 07:03:19 -0400

    Add return to autho middleware

--- server/middleware/authorization/index.js
+++ server/middleware/authorization/index.js
@@ -2,7 +2,8 @@ const { USER_ACCESS_LEVELS } = require('../../../constants');
 
 const isAdmin = () => {
   return async (req, res, next) => {
-    if (!req.user?.admin) res.status(401).json({ error: 'Access denied' });
+    if (!req.user?.admin)
+      return res.status(401).json({ error: 'Access denied' });
     next();
   };
 };
