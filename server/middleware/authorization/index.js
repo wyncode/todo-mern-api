6a04221637a571fc7df9b8bb60ab900c4364e184
Leo <lpolicastro@pm.me>
2020-07-16 08:18:36 -0400

    Clean up authorization

--- server/middleware/authorization/index.js
+++ server/middleware/authorization/index.js
@@ -1,8 +1,6 @@
-const { USER_ACCESS_LEVELS } = require('../../../constants');
-
 const isAdmin = () => {
   return async (req, res, next) => {
-    if (!req.user?.admin)
+    if (!req.user.admin)
       return res.status(401).json({ error: 'Access denied' });
     next();
   };
