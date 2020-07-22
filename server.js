0a9799248882ce532eaf8ba05d5184fe2d399b08
Leo <lpolicastro@pm.me>
2020-07-22 10:58:32 -0400

    clean up

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
 const app = require('./server/app');
 const port = process.env.PORT || 8080;
 
